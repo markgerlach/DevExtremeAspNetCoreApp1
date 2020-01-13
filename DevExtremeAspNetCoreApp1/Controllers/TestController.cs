b80dd2ef05dced33f3c132148bd52c5797668566
markgerlach <mark@gerlach5.com>
2020-01-12 20:32:18 -0800

    test

--- DevExtremeAspNetCoreApp1/Controllers/TestController.cs
+++ DevExtremeAspNetCoreApp1/Controllers/TestController.cs
@@ -10,7 +10,7 @@ namespace DevExtremeAspNetCoreApp1.Controllers
     {
         public IActionResult Index()
         {
-            return View();//
+            return View();
         }
     }
 }
